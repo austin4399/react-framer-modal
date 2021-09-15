113a0a96b764e2530f584a39e2977059886d82a8
austin4399 <Austin4399@gmail.com>
2021-09-15 15:02:41 -0400

    made modal animate

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,42 @@
+import {motion} from "framer-motion"
+import Backdrop from "../Backdrop/index"
+
+const dropIn = {
+    hiddn: {
+        y: "-100vh",
+    },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 0.1,
+            type: "spring",
+            damping: 25,
+        },
+    },
+    exit: {
+        y: "100vh",
+        opacity: 0,
+    }
+};
+
+const Modal = ({handleClose, text}) => {
+
+    return (
+        <Backdrop onClick={handleClose}>
+            <motion.div
+                onClick={(e)=> e.stopPropagation()}
+                className='modal orange-gradient'
+                variants={dropIn}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+            >
+                <p>{text}</p>
+                <button onClick={handleClose}>Close</button>
+            </motion.div>
+        </Backdrop>
+    );
+};
+
+export default Modal;
